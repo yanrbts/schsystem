a4cc0d236ac7c9368f2c45cf7b7bd90aea7c1647
yanrbts <772166784@qq.com>
2025-08-06 18:30:41 +0800

    init

--- apps/models.py
+++ apps/models.py
@@ -5,6 +5,7 @@ Copyright (c) 2019 - present AppSeed.us
 
 from apps import db
 from sqlalchemy.exc import SQLAlchemyError
+from sqlalchemy.orm import sessionmaker, relationship
 from apps.exceptions.exception import InvalidUsage
 
 class Product(db.Model):
@@ -50,3 +51,37 @@ class Product(db.Model):
             error = str(e.__dict__['orig'])
             raise InvalidUsage(error, 422)
         return
+    
+class Group(db.Model):
+    """
+    Group model for managing user groups
+    """
+    __tablename__ = 'groups'
+
+    id = db.Column(db.Integer, primary_key=True) 
+    name = db.Column(db.String, unique=True, nullable=False)
+    # 定义与 User 表的关系：一个 Group 可以有多个 User
+    # backref='group' 会在 User 模型中自动创建一个 'group' 属性，指向所属的 Group 对象
+    users = relationship("User", backref="group", lazy="joined")
+
+    def __repr__(self):
+        return f"<Group(id={self.id}, name='{self.name}')>"
+
+class User(db.Model):
+    """
+    定义 User 表，包含用户基本信息和所属组，以及 IP 地址。
+    """
+    __tablename__ = 'users'
+
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String, unique=True, nullable=False)
+    ip_address = db.Column(db.String(45), nullable=False)
+    devidce_id = db.Column(db.Integer, unique=True, nullable=False)
+
+    # 定义外键，关联到 groups 表的 id 字段
+    # ondelete="SET NULL" 表示当关联的 Group 被删除时，该组下的 User 的 group_id 将被设置为 NULL
+    # nullable=True 必须设置为 True，因为 group_id 现在可以为 NULL
+    group_id = db.Column(db.Integer, db.ForeignKey('groups.id', ondelete="SET NULL"), nullable=True)
+
+    def __repr__(self):
+        return f"<User(id={self.id}, username='{self.username}', ip_address='{self.ip_address}', group_id={self.group_id})>"
